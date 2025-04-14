8d4646787bd2bebb96ee2bcee61dbc5fd633afb3
tenor2000 <tenor2000@gmail.com>
2025-04-14 14:50:33 -0400

    updated wizard route put and delete

--- src/routes/warbands.js
+++ src/routes/warbands.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const getDataFromSource = require("../utilityFuncs/getDataFromSource");
-const newWizardTemplate = require("../utilityFuncs/newTemplates");
-const newApprenticeTemplate = require("../utilityFuncs/newTemplates");
-const generateNewId = require("../utilityFuncs/generateNewId");
-const writeKeyValueToJson = require("../utilityFuncs/writeKeyValuetoJson");
-const deleteKeyFromJson = require("../utilityFuncs/deleteKeyFromJson");
-const isValidMatchingObject = require("../utilityFuncs/isValidMatchingObject");
+const {
+  newPersonnelTemplate,
+  newWizardTemplate,
+  newApprenticeTemplate,
+} = require("../utilityFuncs/newTemplates");
 
+const generateNewId = require("../utilityFuncs/generateNewId");
+const writeObjectToJson = require("../utilityFuncs/writeObjectToJson");
+const deleteObjectFromJson = require("../utilityFuncs/deleteObjectFromJson");
+const replaceObjectInJson = require("../utilityFuncs/replaceObjectInJson");
+const isValidMatchingObject = require("../utilityFuncs/isValidMatchingObject");
 const error = require("../utilityFuncs/error");
+
 const apprenticesData = require("../testData/warbandData/apprentices.json");
 const wizardsData = require("../testData/warbandData/wizards.json");
+const personnelData = require("../testData/warbandData/personnel.json");
 
 // api/warbands/
 
@@ -27,30 +33,22 @@ router
     warbandData ? res.json(warbandData) : next(error(404, "No Data Found"));
   })
   .post((req, res, next) => {
-    // get form data and assign a random id
+    // get form data and assign a object to wizardId
     const formData = req.body;
     // console.log(formData);
 
-    const newApprenticeId = generateNewId("apprentice");
-    const wizardId = formData.wizardId;
+    const wizardId = formData.id;
 
-    if (wizardsData[wizardId]) {
-      wizardsData[wizardId].apprenticeIds = newApprenticeId;
-    } else {
+    if (!wizardsData.find((w) => w.wizardId == wizardId)) {
       return next(error(404, "Wizard not found"));
     }
 
     const apprenticeObject = newApprenticeTemplate(formData.name);
+    apprenticeObject.wizardId = wizardId;
 
-    writeKeyValueToJson(
+    writeObjectToJson(
       "../testData/warbandData/apprentices.json",
-      newApprenticeId,
       apprenticeObject
-    );
-    writeKeyValueToJson(
-      "../testData/warbandData/wizards.json",
-      wizardId,
-      wizardsData[wizardId]
     );
     res.status(201).json(apprenticeObject);
   });
@@ -58,19 +56,25 @@ router
 router
   .route("/apprentices/:id")
   .get((req, res, next) => {
-    const apprenticeId = req.params.id;
+    const wizardId = req.params.id;
 
-    apprenticesData[apprenticeId]
-      ? res.json(apprenticesData[apprenticeId])
-      : next(error(404, "No Data Found"));
+    const apprentice = apprenticesData.find((a) => a.wizardId == wizardId);
+
+    apprentice
+      ? res.json(apprentice)
+      : next(error(404, "No Data Found with that ID"));
   })
   .put((req, res, next) => {
     // formData should be an entire apprentice object, when changes are made
     const formData = req.body;
 
-    if (isValidMatchingObject(apprenticesData["_TEMPLATE_"], formData)) {
-      apprenticesData[req.params.id] = formData;
-      writeKeyValueToJson(
+    const apprenticeObject = apprenticesData.find(
+      (a) => a.wizardId == req.params.id
+    );
+
+    if (isValidMatchingObject(apprenticeObject, formData)) {
+      apprenticeObject[wizardId] = req.params.id;
+      writeObjectToJson(
         "../testData/warbandData/apprentices.json",
         req.params.id,
         formData
@@ -81,7 +85,7 @@ router
     }
   })
   .delete((req, res, next) => {
-    const apprenticeId = req.params.id;
+    const wizardId = req.params.id;
     // WIP
   });
 
@@ -97,7 +101,7 @@ router
   .post((req, res, next) => {
     // get form data and assign a random id
     const formData = req.body;
-    // console.log(formData);
+    console.log(formData);
 
     const newWizardId = generateNewId("wizard");
 
@@ -110,32 +114,34 @@ router
       formData.neutralSpellIds,
       formData.backstory
     );
-    writeKeyValueToJson(
-      "../testData/warbandData/wizards.json",
-      newWizardId,
-      wizardObject
-    );
+    console.log(wizardObject);
+
+    wizardObject._id = newWizardId;
+    writeObjectToJson("../testData/warbandData/wizards.json", wizardObject);
     res.status(201).json(wizardObject);
   });
 
 router
   .route("/wizards/:id")
   .get((req, res, next) => {
-    const wizardId = req.params.id;
+    const wizard_id = req.params.id;
 
-    wizardsData[wizardId]
-      ? res.json(wizardsData[wizardId])
-      : next(error(404, "No Data Found"));
+    const wizardObject = wizardsData.find((w) => w.wizard_id == wizard_id);
+
+    wizardObject ? res.json(wizardObject) : next(error(404, "No Data Found"));
   })
   .put((req, res, next) => {
     // formData should be an entire wizard object, when changes are made
     const formData = req.body;
 
-    if (isValidMatchingObject(wizardsData["_TEMPLATE_"], formData)) {
-      wizardsData[req.params.id] = formData;
-      writeKeyValueToJson(
+    const wizardObject = wizardsData.find(
+      (w) => w.wizard_id == req.body.wizard_id
+    );
+
+    if (wizardObject && isValidMatchingObject(wizardObject, formData)) {
+      replaceObjectInJson(
         "../testData/warbandData/wizards.json",
-        req.params.id,
+        wizardObject,
         formData
       );
       res.status(201).json(formData);
@@ -145,36 +151,59 @@ router
   })
   .delete((req, res, next) => {
     //WIP
-    const wizardId = req.params.id;
+    const wizard_id = req.params.id;
 
-    if (wizardsData[wizardId]) {
-      deleteKeyFromJson("../testData/warbandData/wizards.json", wizardId);
-      res.status(201).json(wizardsData[wizardId]);
+    const wizardObject = wizardsData.find((w) => w.wizard_id == wizard_id);
+
+    if (wizardObject) {
+      deleteObjectFromJson("../testData/warbandData/wizards.json", wizard_id);
+      res.status(201).json(wizardObject);
     } else {
-      return next(error(404, "Wizard not found"));
+      next(error(404, "Wizard not found"));
     }
   });
 
 router
-  .route("/rosters")
+  .route("/personnel")
   .get(async (req, res, next) => {
-    const warbandData = await getDataFromSource("warbandData", "rosters");
+    const warbandData = await getDataFromSource("warbandData", "personnel");
     warbandData ? res.json(warbandData) : next(error(404, "No Data Found"));
   })
   .post((req, res, next) => {
     // get form data and assign a random id
     const formData = req.body;
-    // WIP
+    // console.log(formData);
+
+    // const newRosterId = generateNewId("personnel");
+    const wizardId = formData.wizardId;
+
+    if (!wizardsData.find((w) => w.wizardId == wizardId)) {
+      next(error(404, "Wizard not found"));
+    }
+
+    const personnelObject = newPersonnelTemplate(formData.name, wizardId);
+
+    writeKeyValueToJson(
+      "../testData/warbandData/apprentices.json",
+      newApprenticeId,
+      apprenticeObject
+    );
+    writeKeyValueToJson(
+      "../testData/warbandData/wizards.json",
+      wizardId,
+      wizardsData[wizardId]
+    );
+    res.status(201).json(apprenticeObject);
   });
 
 router
-  .route("/rosters/:id")
+  .route("/personnel/:id")
   .get((req, res, next) => {
-    const rosterId = req.params.id;
+    const wizardId = req.params.id;
 
-    rostersData[rosterId]
-      ? res.json(rostersData[rosterId])
-      : next(error(404, "No Data Found"));
+    const personnel = personnelData.filter((p) => p.wizardId == wizardId);
+
+    personnel ? res.json(personnel) : next(error(404, "No Data Found"));
   })
   .put((req, res, next) => {
     //WIP
