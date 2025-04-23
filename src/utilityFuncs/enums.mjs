3cd88daccfbb660d6a6d6943d9f6577a40d20de9
tenor2000 <tenor2000@gmail.com>
2025-04-23 15:27:11 -0400

    created enums for simple definitions tables

---
+++ src/utilityFuncs/enums.mjs
@@ -0,0 +1,14 @@
+export const StateLabels = {
+  1: "current",
+  2: "dropped",
+  3: "killed",
+};
+
+export const StatusLabels = {
+  0: "Dead",
+  1: "Active",
+  2: "Badly Wounded",
+  7: "Hired",
+  8: "For Hire",
+  9: "Vacant",
+};
