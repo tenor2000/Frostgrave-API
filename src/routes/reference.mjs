7ad9ee4fc7278f84cb7ae85d06b4fc2b973778ef
tenor2000 <tenor2000@gmail.com>
2025-04-24 15:36:03 -0400

    fixed to use new helper versatile func

--- src/routes/reference.mjs
+++ src/routes/reference.mjs
@@ -1,36 +1,23 @@
 import express from "express";
 import error from "../utilityFuncs/error.mjs";
-import getDataFromSource from "../utilityFuncs/getDataFromSource.mjs";
-
+import getModelsFromDirectory from "../utilityFuncs/getModelsFromDirectory.mjs";
 const router = express.Router();
-
-import ArmorModel from "../models/reference/armor.model.mjs";
-import WeaponModel from "../models/reference/weapon.model.mjs";
-import CreatureModel from "../models/reference/creature.model.mjs";
-import MagicSchoolModel from "../models/reference/magicSchool.model.mjs";
-import SpellModel from "../models/reference/spell.model.mjs";
-import SoldierModel from "../models/reference/soldier.model.mjs";
-
-const modelMap = {
-  armor: ArmorModel,
-  weapon: WeaponModel,
-  creature: CreatureModel,
-  magicSchool: MagicSchoolModel,
-  spell: SpellModel,
-  soldier: SoldierModel,
-};
 
 // api/reference/
 
 router.route("/data").get(async (req, res, next) => {
   const searchtype = req.query.type || null;
-  console.log(searchtype);
+  // Retrieve reference data
 
   try {
-    const referenceData = await getDataFromSource("reference", searchtype);
+    const models = await getModelsFromDirectory("reference", searchtype);
+    let referenceData = {};
+    for (const [key, value] of Object.entries(models)) {
+      referenceData[key + "_data"] = await value.find();
+    }
     referenceData ? res.json(referenceData) : next(error(404, "No Data Found"));
   } catch (err) {
-    console.error(`Error inserting ${type}:`, err);
+    console.error(`Error retrieving ${type}:`, err);
     res.status(500).json({ status: 500, error: err.message, details: err });
   }
 });
@@ -38,12 +25,25 @@ router.route("/data").get(async (req, res, next) => {
 router
   .route("/data/:type")
   .get(async (req, res, next) => {
-    const referenceData = await getDataFromSource("reference", req.params.type);
-    referenceData ? res.json(referenceData) : next(error(404, "No Data Found"));
+    try {
+      const models = await getModelsFromDirectory("reference", req.params.type);
+      let referenceData = {};
+      referenceData[req.params.type + "_data"] = await models[
+        req.params.type
+      ].find();
+
+      referenceData
+        ? res.json(referenceData)
+        : next(error(404, "No Data Found"));
+    } catch (err) {
+      console.error(`Error retrieving ${type}:`, err);
+      res.status(500).json({ status: 500, error: err.message, details: err });
+    }
   })
   .post(async (req, res, next) => {
     // Used for seeding
-    const { type } = req.params;
+    const modelMap = await getModelsFromDirectory("reference");
+    const type = req.params.type;
     const data = req.body;
 
     const Model = modelMap[type];
@@ -51,14 +51,14 @@ router
     if (!Model) {
       return res
         .status(404)
-        .json({ error: `No model found for type: ${modelType}` });
+        .json({ error: `No model found for type: ${type}` });
     }
 
     try {
       const result = await Model.create(data);
       res.json(result);
     } catch (err) {
-      console.error(`Error inserting ${modelType}:`, err);
+      console.error(`Error inserting ${type}:`, err);
       res.status(500).json({ error: err.message, details: err });
     }
   });
