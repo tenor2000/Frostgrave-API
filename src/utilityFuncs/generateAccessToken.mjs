6fb50b4cadfd85c8b5753baff56f42a8ed04e76a
tenor2000 <tenor2000@gmail.com>
2025-05-15 03:12:55 -0400

    conosle messaging

--- src/utilityFuncs/generateAccessToken.mjs
+++ src/utilityFuncs/generateAccessToken.mjs
@@ -1,8 +1,11 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const jwtExpireTime = "2h";
 
 export default function generateAccessToken(user) {
+  console.log("Generating access token for:", user);
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: jwtExpireTime,
   });
